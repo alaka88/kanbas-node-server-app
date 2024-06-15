991f46a5fb72bb7c40d0f7c87be89381eb93b2c9
zhim <wang.zhim@northeastern.edu>
2024-06-15 04:14:53 -0400

    session

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config";
 import express from 'express';
+import session from "express-session";
 import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
@@ -9,11 +10,33 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import QuizRoutes from "./Kanbas/Quizzes/routes.js";
 import Lab5 from "./Lab5/index.js";
 import UserRoutes from "./Users/routes.js";
+
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-su1"
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+        }
+));
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }  
+  app.use(
+    session(sessionOptions)
+  );  
 app.use(express.json());
 UserRoutes(app);
 CourseRoutes(app);
